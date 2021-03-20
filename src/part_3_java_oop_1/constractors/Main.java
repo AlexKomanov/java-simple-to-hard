487be0627a30175643e08836068f5a472b812025
alexkomanov <ALexko26>
2021-03-20 15:06:28 +0200

    AK - Constructors

---
+++ src/part_3_java_oop_1/constractors/Main.java
@@ -0,0 +1,10 @@
+package part_3_java_oop_1.constractors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("Alex", "Developer", 100);
+        System.out.println(employee);
+
+    }
+}
---
+++ src/part_3_java_oop_1/constractors/StaticImports.java
@@ -0,0 +1,16 @@
+package part_3_java_oop_1.constractors;
+
+import static java.lang.Math.*;
+
+public class StaticImports {
+
+    public static void main(String[] args) {
+
+        double sqrt = sqrt(9);
+        double sqrt2 = sqrt(25);
+        double cos = cos(45);
+        System.out.println(sqrt);
+        System.out.println(sqrt2);
+        System.out.println(cos);
+    }
+}
